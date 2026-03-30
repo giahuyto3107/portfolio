4f581f3715e4f1c63f24708a88aa3d7ca0cd0c5f
giahuyto3107 <giahuyto3107@gmail.com>
2026-03-30 14:05:20 +0700

    style (project_section): modify projects' description

--- lib/features/projects/data/models/project.dart
+++ lib/features/projects/data/models/project.dart
@@ -1,17 +1,31 @@
+class SubLinks {
+  final String feLink;
+  final String beLink;
+
+  const SubLinks({
+    required this.feLink,
+    required this.beLink
+  });
+}
+
 class Project {
   final String coverImagePath;
   final String title;
   final String description;
+  final bool isCover;
+  final SubLinks? subLinks;
   final List<String> techStack;
-  final Function() onCodeTap;
+  final Function()? onCodeTap;
   final Function() onDemoTap;
 
   Project({
+    this.isCover = true,
+    this.subLinks,
     required this.coverImagePath,
     required this.title,
     required this.description,
     required this.techStack,
-    required this.onCodeTap,
+    this.onCodeTap,
     required this.onDemoTap
   });
 }
